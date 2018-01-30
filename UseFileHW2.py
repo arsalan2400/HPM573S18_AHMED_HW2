77de64b21a4e4c96af0c951a276fd8b19138a891
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-01-30 15:18:47 -0500

    Update UseFileHW2.py

--- UseFileHW2.py
+++ UseFileHW2.py
@@ -9,9 +9,3 @@ print(usefile.superhealthyscore(2,1,2,3,1,4,5,3))
 print("This is a test score where all categories were 1. The patient's score is ", 1,1,1,1,1,1,1,1)
 print(usefile.superhealthyscore(1,1,1,1,1,1,1,1))
 #output is 1.0
-
-
-#Should give 0.000?
-print("This is a test score where all categories were 5 or 6 (whatever is max). The patient's score is ", 6,6,5,6,6,5,6,5)
-print(usefile.superhealthyscore(6,6,5,6,6,5,6,5))
-#it gave me -0.35902730636441177. What's going on?
